1b788c8cf53308e8cd6c9a0c177e7c6ff1ae02ce
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-10 18:19:14 -0500

    Se configura servidor express y se agrega git ignore

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola de nuevo!!');
+const express = require('express');
+
+// Crear el servidor/aplicacion de express
+const app = express();
+
+
+//GET
+app.get('/', (req, res) => {
+    //res.status(404).json({
+    res.json({
+        ok: true,
+        msg: 'Todo salio bien',
+        uid: 12345
+    })
+})
+
+// Levantar aplicacion, para escuchar cualquier informacion que venga hacia algun puerto en especifico
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);
+});
